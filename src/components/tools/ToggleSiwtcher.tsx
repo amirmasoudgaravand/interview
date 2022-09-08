ad8d2d978e716371fa8637f57c31acc345bf34d2
amirmasoudgaravand <garavandamirmasoud@gmail.com>
2022-09-08 12:32:24 -0700

    toggle siwtcher

---
+++ src/components/tools/ToggleSiwtcher.tsx
@@ -0,0 +1,32 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../state-management/store";
+interface ToggleSwitcherProps {
+  label: string;
+  onChange: any;
+  disabled?: boolean;
+}
+
+const ToggleSwitcher = (props: ToggleSwitcherProps) => {
+	const toggleState = useSelector((state: RootState) => state.from.value);
+	const simcardtype = toggleState[0].value;
+  return (
+    <div className="container">
+      <div className="toggle-switch">
+        <input
+          disabled={props.disabled}
+          type="checkbox"
+          className="checkbox"
+          name={props.label}
+          id={props.label}
+          onChange={props.onChange}
+        />
+        <label className="label" htmlFor={props.label}>
+          <span className={simcardtype !== "credit"? "default_toggle_inner disabled_switch inner" :"inner"} />
+          <span className={simcardtype !== "credit"? "default_toggle_switch cursor_disabled switch" :"switch"}  />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default ToggleSwitcher;
