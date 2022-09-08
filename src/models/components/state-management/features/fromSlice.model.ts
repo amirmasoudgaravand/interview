399874cb098c50a926eb9baf2a4ce53f2893502c
amirmasoudgaravand <garavandamirmasoud@gmail.com>
2022-09-08 12:34:00 -0700

    form slice model and toggle slice model

---
+++ src/models/components/state-management/features/fromSlice.model.ts
@@ -0,0 +1,11 @@
+export interface formSliceModel {
+    value: [
+      { name: string; value: string },
+      { name: string; value: string },
+      { name: string; value: string },
+      { name: string; value: string },
+      { name: string; value: string },
+      { name: string; value: string },
+      { name: string; value: string },
+    ];
+  }
